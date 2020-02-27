8bfe595b479fd1b964c88457eea5a1baed91fd6d
paulwalk <paul@paulwalk.net>
2020-02-27 12:44:06 +0000

    configured technology table

--- app/models/adoption_level.rb
+++ app/models/adoption_level.rb
@@ -1,4 +1,6 @@
 class AdoptionLevel < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+
+  has_many :technologies
 end
--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -15,7 +15,7 @@ class ApplicationPolicy
   end
 
   def create?
-    false
+    @user != nil
   end
 
   def new?
@@ -23,7 +23,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    @user != nil
   end
 
   def edit?
@@ -31,7 +31,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    @user != nil
   end
 
   class Scope
