7e013b04631f043a5e7aac8dcde4c25b9b812b5f
paulwalk <paul@paulwalk.net>
2020-02-28 17:07:59 +0000

    added authorisation

--- app/javascript/packs/scomcat.js
+++ app/javascript/packs/scomcat.js
@@ -1,34 +1,46 @@
-$(document).ready(function(){
-	$('.unsorted-table').dataTable({
-		"bSort":false,
-		"bInfo":false,
-		"bFilter":false,
-		"bPaginate":false,
-		"fixedHeader": false,
-		"order": [[ 0, "asc" ]]
-	});
+$(document).ready(function () {
+    $('.unsorted-table').dataTable({
+        "bSort": false,
+        "bInfo": false,
+        "bFilter": false,
+        "bPaginate": false,
+        "fixedHeader": false,
+        "order": [[0, "asc"]]
+    });
 });
 
-$(document).ready(function(){
-	$('.sorted-table').dataTable({
-		"bSort":true,
-		"bInfo":false,
-		"bFilter":false,
-		"bPaginate":false,
-		"fixedHeader": false,
-		"order": [[ 0, "asc" ]],
-		"columnDefs": [{targets: 'actions', orderable: false }],
-		stateSave: true
-	});
+$(document).ready(function () {
+    $('.sorted-table').dataTable({
+        "bSort": true,
+        "bInfo": false,
+        "bFilter": false,
+        "bPaginate": false,
+        "fixedHeader": false,
+        "order": [[0, "asc"]],
+        "columnDefs": [{targets: 'actions', orderable: false}],
+        stateSave: true
+    });
 });
 
-$(document).ready(function(){
-	$('.reverse-sorted-table').dataTable({
-		"bSort":true,
-		"bInfo":false,
-		"bFilter":false,
-		"bPaginate":false,
-		"fixedHeader": false,
-		"order": [[ 0, "desc" ]]
-	});
+$(document).ready(function () {
+    $('.reverse-sorted-table').dataTable({
+        "bSort": true,
+        "bInfo": false,
+        "bFilter": false,
+        "bPaginate": false,
+        "fixedHeader": false,
+        "order": [[0, "desc"]]
+    });
 });
+
+$('document').ready(function() {
+	setTimeout(function() {
+		$('.alert-success').slideUp();
+	}, 3000);
+});
+
+// $(document).ready(function () {
+//     $(".alert").fadeTo(2000, 500).slideUp(500, function () {
+//         $(".alert").slideUp(500);
+//     });
+// });
