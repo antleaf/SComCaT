d755f62a49b7a6ff00a4913820b011ff5fd342af
paulwalk <paul@paulwalk.net>
2020-11-18 16:16:51 +0000

    Configuration changes, and wrote deployment instructions

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # ScomCat
 Scholarly Communications Technology Catalogue
+
+## To deploy
+
+### Install all necessary ruby gems
+Run `bundle install`
+
+### Set environment variables
+Create a file in this root folder called `.env` by copying `.env_TEMPLATE`
+
+The first section of this configuration is **necessary** to configure the SMTP mailer used to:
+* send "forgotten password" messages
+* send user feedback to the editors
+
+A GMail account works just fine for this - but you are advised to get an 'application password' for this account rather than using the user account's password.
+
+[Read more about application passwords for Google accounts here](https://support.google.com/accounts/answer/185833?hl=en)
+
+The second section is optional, and mainly demonstrates how to configure a DB with ENV variables.
+
+### Configure the database
+SComCat should work just fine with any relational database system supported with an `ActiveRecord` adapter.
+
+1. Create a database configuration file at `./config/database.yml` - several templates for different database systems are in the config folder
+2. Create the database(s): `rails db:create`
+3. Migrate the database(s): `rails db:migrate`
+
+### Configure an admin account and seed the database
+Edit the file at `./db/seeds.rb`
+
+Uncomment this section:
+```ruby
+user = User.new
+user.email = ''
+user.forenames = ''
+user.lastname = ''
+user.password = ''
+user.password_confirmation = ''
+user.roles << admin_role
+user.roles << edit_role
+user.roles << helpdesk_role
+user.save!
+```
+and set values for the admin user for `email`, `forenames`, `lastname`, `password` and `password_confirmation`
+
+Save your changes to `./db/seeds.rb` and run `rails db:seed`
+
+### Start the application
+Run `rails server`
+
+If running in development mode, go to `http://localhost:3000`
+
+You can login withe the account you configured in `./db/seeds.rb`. Now you can create more user accounts using the admin web interface.
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,71 +1,29 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'csv'
-# require './db/remote_google_data'
 
+admin_role = Role.create(
+    name: 'Admin',
+    description: 'Full administration rights'
+)
 
-CSV_PARSING_OPTIONS = {:headers=>true,:encoding=>'UTF-8'}
-CSV_DATA_ROOT = "#{File.dirname(__FILE__)}/seed_data"
+edit_role = Role.create(
+    name: 'Editor',
+    description: 'Can edit but not delete records'
+)
 
-# models = [AdoptionLevel,Assignment,BusinessForm,Category,Function,Governance,ReadinessLevel,Relationship,Role,Status,Technology,User]
-
-# models.each do |model|
-#   CSV.open( "#{CSV_DATA_ROOT}/#{model.name}.csv", 'w' ) do |csv|
-#     records = model.all
-#     csv << records.first.attributes.map { |a,v| a }
-#     records.each do |record|
-#       csv << record.attributes.map { |a,v| v }
-#     end
-#   end
-# end
-
-# csv = CSV.parse(File.read("#{CSV_DATA_ROOT}/Assignment.csv"), :headers => true, :encoding => 'UTF-8')
-# csv.each do |row|
-#   puts row.to_hash
-# end
-
-
-
-
-
-# CSV.open("#{CSV_DATA_ROOT}/technologies.csv", CSV_PARSING_OPTIONS).each do |row|
-#   tech = Technology.find(row['id'])
-#   tech.adoption_level = AdoptionLevel.find_by_slug(row['adoption_level'])
-#   tech.readiness_level = ReadinessLevel.find_by_slug(row['readiness_level'])
-#   tech.status = Status.find_by_slug(row['status'])
-#   tech.governance = Governance.find_by_slug(row['governance'])
-#   tech.business_form = BusinessForm.find_by_slug(row['business_form'])
-#   if row['base_tech_list']
-#     tech.base_tech_list = row['base_tech_list'].strip
-#   end
-#   if row['functions']
-#     tech.functions.destroy_all
-#     row['functions'].split(',').each do |function_slug|
-#       tech.functions << Function.find_by_slug(function_slug.strip)
-#     end
-#   end
-#   if row['categories']
-#     tech.categories.destroy_all
-#     row['categories'].split(',').each do |category_slug|
-#       tech.categories << Category.find_by_slug(category_slug.strip)
-#     end
-#   end
-#   tech.hosting = row['hosting']
-#   tech.url = row['url']
-#   tech.description = row['description']
-#   tech.pricing = row['pricing']
-#   tech.roadmap = row['roadmap']
-#   tech.licensing = row['licensing']
-#   tech.codebase = row['codebase']
-#   puts "saving #{tech.name}..."
-#   tech.save!
-# end
-
+helpdesk_role = Role.create(
+    name: 'Helpdesk',
+    description: 'Receives and handles feedback from users'
+)
 
 # user = User.new
-# user.email = ENV["ADMIN_ACCOUNT_EMAIL"]
-# user.password = ENV["ADMIN_ACCOUNT_PASSWORD"]
-# user.password_confirmation = ENV["ADMIN_ACCOUNT_PASSWORD"]
+# user.email = ''
+# user.forenames = ''
+# user.lastname = ''
+# user.password = ''
+# user.password_confirmation = ''
 # user.roles << admin_role
 # user.roles << edit_role
+# user.roles << helpdesk_role
 # user.save!
