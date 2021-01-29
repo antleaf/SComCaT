3fa43816d4040ee0cb5568374bbe23ee6c15deaa
paulwalk <paul@paulwalk.net>
2021-01-29 10:18:15 +0000

    added delete option to show view

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -18,6 +18,7 @@ class Technology < ApplicationRecord
   has_many :dependencies, dependent: :destroy
   has_many :dependees, through: :dependencies
 
+  default_scope { order(slug: :asc) }
   scope :published, -> { where(editorial_state: EditorialState.friendly.find('published')) }
   scope :draft, -> { where(editorial_state: EditorialState.friendly.find('draft')) }
 
