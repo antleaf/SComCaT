130313b874a8dc755739ac8e6c2b06fd504c0be4
paulwalk <paul@paulwalk.net>
2021-03-04 15:37:10 +0000

    swapped to Freshdesk feedback

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,15 +1,23 @@
 class FeedbackController < ApplicationController
   def index
-    Role.find_by_name('Helpdesk').users.each do |user|
-      FeedbackMailer.with(
-          recipient: user.email,
-          calling_page: params['calling_page'],
-          name: params['name'],
-          email_address: params['email_address'],
-          body: params[:body],
-          technology_id: params[:technology_id]
-      ).feedback_email.deliver_now
-    end
-    redirect_back(fallback_location: root_path, notice: 'Your feedback has been sent to the editors.')
+    # Role.find_by_name('Helpdesk').users.each do |user|
+    #   FeedbackMailer.with(
+    #       recipient: user.email,
+    #       calling_page: params['calling_page'],
+    #       name: params['name'],
+    #       email_address: params['email_address'],
+    #       body: params[:body],
+    #       technology_id: params[:technology_id]
+    #   ).feedback_email.deliver_now
+    # end
+    # FeedbackMailer.with(
+    #   recipient: ENV['SCOMCAT_FEEDBACK_TARGET_EMAIL_ADDRESS'],
+    #   calling_page: params['calling_page'],
+    #   name: params['name'],
+    #   email_address: params['email_address'],
+    #   body: params[:body],
+    #   technology_id: params[:technology_id]
+    # ).feedback_email.deliver_now
+    # redirect_back(fallback_location: root_path, notice: 'Your feedback has been sent to the editors.')
   end
 end
